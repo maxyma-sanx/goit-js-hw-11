6bb31ab03d8fba6697a83b95ca62ce9d365f411f
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-12-18 00:19:36 +0200

    refs fix

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,4 @@
-export default refs = {
+export default {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   button: document.querySelector('.load-more'),
