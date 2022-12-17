8a4100f79f1beed7eca00883f701c38f860f93c3
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-12-18 00:17:32 +0200

    hw-11

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ const pixabay = new PixabayAPI();
 refs.form.addEventListener('submit', onSearchFormSubmit);
 refs.button.addEventListener('click', onButtonLoadMoreClick);
 
-async function onSearchFormSubmit(e) {
+function onSearchFormSubmit(e) {
   e.preventDefault();
 
   const { searchQuery } = e.currentTarget.elements;
@@ -22,23 +22,73 @@ async function onSearchFormSubmit(e) {
     return;
   }
 
-  const images = await pixabay.getImages();
+  getImages();
+  clear();
 
-  if (images.hits.length === 0) {
-    Notify.warning(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-    return;
+  e.target.reset();
+}
+
+async function getImages() {
+  try {
+    const images = await pixabay.getData();
+
+    if (images.hits.length === 0) {
+      refs.button.classList.add('is-hidden');
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    pixabay.calculatePages(images.totalHits);
+
+    refs.gallery.insertAdjacentHTML('beforeend', renderImages(images));
+
+    if (pixabay.isShowLoadButton()) {
+      refs.button.classList.remove('is-hidden');
+    }
+
+    Notify.success(`Hooray! We found ${images.totalHits} images.`);
+
+    const lightbox = new SimpleLightbox('.gallery .photo-card a');
+  } catch (error) {
+    console.log(error);
   }
-
-  refs.gallery.insertAdjacentHTML('beforeend', renderImages(images));
-  Notify.success(`Hooray! We found ${images.totalHits} images.`);
-
-  const lightbox = new SimpleLightbox('.gallery .photo-card a');
 }
 
 async function onButtonLoadMoreClick() {
-  pixabay.incrementPage();
-  const images = await pixabay.getImages();
-  refs.gallery.insertAdjacentHTML('beforeend', renderImages(images));
+  try {
+    pixabay.incrementPage();
+
+    const images = await pixabay.getData();
+
+    if (!pixabay.isShowLoadButton()) {
+      refs.button.classList.add('is-hidden');
+    }
+
+    if (pixabay.pagesCheck()) {
+      Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+
+    refs.gallery.insertAdjacentHTML('beforeend', renderImages(images));
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery .photo-card a')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    clear();
+    console.log(error);
+  }
+}
+
+function clear() {
+  pixabay.resetPage();
+  refs.gallery.innerHTML = '';
 }
