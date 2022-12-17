934d9de1b77f5aa80e534e0f2e12648678f429ff
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-12-18 00:17:49 +0200

    hw-11

--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -5,17 +5,19 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 export default class PixabayAPI {
   #searchQuery = '';
   #page = 1;
+  #totalPages = null;
+  #perPage = 40;
   #params = {
     params: {
       key: '32149017-54898b7893ffe9aab4d4c2fa3',
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
-      per_page: '6',
+      per_page: this.#perPage,
     },
   };
 
-  async getImages() {
+  async getData() {
     const url = `?q=${this.#searchQuery}&page=${this.#page}`;
 
     const { data } = await axios.get(url, this.#params);
@@ -33,5 +35,21 @@ export default class PixabayAPI {
 
   incrementPage() {
     this.#page += 1;
+  }
+
+  resetPage() {
+    this.#page = 1;
+  }
+
+  calculatePages(total) {
+    this.#totalPages = Math.ceil(total / this.#perPage);
+  }
+
+  pagesCheck() {
+    return this.#page === this.#totalPages;
+  }
+
+  isShowLoadButton() {
+    return this.#page < this.#totalPages;
   }
 }
